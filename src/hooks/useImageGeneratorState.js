5329e59c6de149e58b90dba329644d430054db1c
Rabden <163085056+rabden@users.noreply.github.com>
2025-06-03 20:11:28 +0600

    Update useImageGeneratorState.js

--- src/hooks/useImageGeneratorState.js
+++ src/hooks/useImageGeneratorState.js
@@ -11,7 +11,7 @@ export const useImageGeneratorState = () => {
     randomizeSeed: true,
     width: 1024,
     height: 1024,
-    model: 'flux', // Using flux as default
+    model: 'flash',
     activeTab: 'images',
     aspectRatio: '1:1',
     useAspectRatio: true,
